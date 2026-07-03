1b07efcd4fd32a95feb28649caf4bd5c52c78f0d
Sabera Subhani <saberasubhanihimu@gmail.com>
2026-07-03 23:10:14 +0600

    model import fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import User from "../models/User";
+import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
@@ -7,13 +7,8 @@ const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    const {
-      id,
-      first_name,
-      last_name,
-      email_addresses,
-      image_url,
-    } = event.data;
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
 
     const userData = {
       _id: id,
@@ -23,31 +18,32 @@ const syncUserCreation = inngest.createFunction(
     };
 
     await User.create(userData);
-  }
+  },
 );
 
 const syncUserDeletion = inngest.createFunction(
   { id: "delete-user-with-clerk" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
-    const {id} = event.data
-    await User.findByIdAndDelete(id)
-  }
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
+  },
 );
 const syncUserUpdation = inngest.createFunction(
   { id: "update-user-with-clerk" },
   { event: "clerk/user.updated" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } = event.data
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
 
-const userData = {
-  _id: id,
-  email: email_addresses[0].email_address,
-  name: first_name + ' ' + last_name,
-  image: image_url
-}
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
 
-await User.findByIdAndUpdate(id, userData)
-  }
+    await User.findByIdAndUpdate(id, userData);
+  },
 );
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation ]; 
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
